05d132852e53cb6f2ee062e4ac96a710ae81b9e4
ngbin <ngbin@DESKTOP-GB9K06E>
2021-07-15 19:36:04 +0700

    Add project files.

---
+++ Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ADKT_WebProject.MvcApplication" Language="C#" %>
---
+++ Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ADKT_WebProject.Startup))]
+namespace ADKT_WebProject
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
